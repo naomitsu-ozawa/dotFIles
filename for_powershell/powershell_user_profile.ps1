0373dcb8b02c901475c0571a3bf0ed2494f031d9
naomitsu-ozawa <ozawa_naomitsu@kurume-u.ac.jp>
2024-10-08 10:43:47 +0900

    update

--- for_powershell/powershell_user_profile.ps1
+++ for_powershell/powershell_user_profile.ps1
@@ -1,16 +1,14 @@
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "C:\Users\eutro\miniconda3\Scripts\conda.exe") {
-    (& "C:\Users\eutro\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-}
-#endregion
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/grandpa-style.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/paradox.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_rainbow.omp.json" | Invoke-Expression
+
 
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 # Import-module posh-git
 
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_rainbow.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_rainbow_2.omp.json" | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
@@ -27,6 +25,7 @@ Set-PSReadLineKeyHandler -Key Tab -Function Complete
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
 Import-Module z
+# Import-Module ZLocation
 
 # Fzf
 Import-Module PSFzf
@@ -35,8 +34,37 @@ Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory
 
 # Alias
 Set-Alias -Name vim -Value nvim
+Set-Alias l ls
 Set-Alias ll ls
 Set-Alias lt tree
+Set-Alias cat Get-Content
+Set-Alias grep Select-String
+# Set-Alias cp Copy-Item
+Set-Alias mv Move-Item
+Set-Alias rm Remove-Item
+Set-Alias man Get-Help
+
+function open {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Path
+    )
+
+    if (Test-Path $Path) {
+        # ファイルやディレクトリが存在する場合、それを開く
+        Start-Process $Path
+    } elseif ($Path -match "^https?:\/\/") {
+        # URL が指定された場合、それをブラウザで開く
+        Start-Process $Path
+    } else {
+        Write-Host "指定されたパスが存在しません: $Path"
+    }
+}
+
+function touch { New-Item -ItemType File -Name ($args -join ' ') }
+
+function la {ls -Force}
+function lla {ls -Force}
 
 # Utilities
 function which ($command) {
